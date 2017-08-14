01b1f355b8a736ee94b5b116b23aaf92477fb44f
KrishnaVangala <saichaitanya6@gmail.com>
2017-08-14 12:42:08 -0500

    Added test for the tomcat on port 8080

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -16,3 +16,9 @@ end
 describe port(80), :skip do
   it { should_not be_listening }
 end
+
+describe 'tomcat::default' do
+  describe command("curl http://localhost:8080") do
+    its(:stdout){ should match /Tomcat/ }
+  end
+end
